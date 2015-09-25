96872756eed82bb6ef3030aca279f3fd11c7819a
Jack Hu <hutufeia2009@sina.com>
2015-09-26 01:07:11 +0800

    sinon and nock

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var express = require('express');
-
+var fs = require('fs');
+var superagent = require('superagent');
 var app = express();
 
 app.set('port', process.env.PORT || 3000 );
@@ -18,6 +19,33 @@ app.get('/article/:id', function (req, res) {
 		return res.status(404).send();
 	}
 });
+
+//获取文章列表
+app.get('/articleList',function (req,res) {
+	var articles = [];
+	fs.readdirSync('./articles').forEach(function (file) {
+		articles.push(fs.readFileSync('./articles/' + file, 'utf8'));
+	});
+	return res.status(200).json(articles);
+});
+
+//获取用户地址
+app.get('/userAddress',function (req,res) {
+	var ip = ((/127\.0\.0\.1/).test(req.ip) || req.ip === '::1')?'210.75.225.254':req.ip;
+	var url = "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip;
+
+	superagent.get(url)
+	.set('Accept', 'application/json')
+	.end(function (err,response) {
+		var infos = JSON.parse(decodeURI(response.text));
+		if(infos.code === 0){
+			return res.status(200).json(infos.data);
+		}else{
+			return res.status(500).send();
+		}
+	});
+});
+
 
 app.listen(app.get('port'),function () {
 	console.log('Express server listening on %d', app.get('port'));
--- test/index.js
+++ test/index.js
@@ -32,3 +32,4 @@ describe('/article/:id', function() {
 			});
 	});
 });
+
